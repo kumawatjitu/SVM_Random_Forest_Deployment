a06736a9025645458c0e25ffeb4c2d5f6e4dc30e
kumawatjitu <89432379+kumawatjitu@users.noreply.github.com>
2022-08-02 11:05:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-model = pickle.load(open('/content/drive/My Drive/svmmodel.pkl', 'rb')) 
-model_randomforest = pickle.load(open('/content/drive/My Drive/randomforest.pkl', 'rb')) 
-dataset= pd.read_csv('/content/drive/My Drive/Social_Network_Ads.csv')
+model = pickle.load(open('svmmodel.pkl', 'rb')) 
+model_randomforest = pickle.load(open('randomforest.pkl', 'rb')) 
+dataset= pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
